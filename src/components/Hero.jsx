2dfebe292fb931b0e051b615fb647c24bbadd5a8
nihal <nihalpuliyakkady@gmail.com>
2025-05-13 23:18:35 +0530

    gtm

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion";
 
+
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import App from "./App";
 import "./index.css";
+
+import TagManager from 'react-gtm-module';
+
+const tagManagerArgs = {
+  gtmId: 'GTM-5KVS2BWQ'
+};
+
+TagManager.initialize(tagManagerArgs);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
